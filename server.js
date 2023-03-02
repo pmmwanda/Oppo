92dd16b1a4c602d783f7729dab76910ba39153b8
Paulinemkavita <114083982+Paulinemkavita@users.noreply.github.com>
2023-03-02 18:19:44 +0300

    added notes on srver js

--- server.js
+++ server.js
@@ -10,3 +10,6 @@ app.listen(3000, function() {
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+})
