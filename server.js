99d0885d9c2339cff2bd37825ed5e32c1454c1ea
Paulinemkavita <114083982+Paulinemkavita@users.noreply.github.com>
2023-03-02 16:42:10 +0300

    added htm file

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 console.log('May Node be with you')
 const express = require('express');
 const app = express();
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+  app.get('/', function(req, res) {
+    res.send('Hello World')
+  })
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
